f01602f645e1f82c80e38bc241007b69f960c3eb
Davin Hong <davin.hong3@gmail.com>
2025-08-17 20:28:09 +0800

    update

--- Github_copy_2/utils/spinner.py
+++
@@ -1,7 +0,0 @@
-from tqdm import tqdm
-import time
-
-# // Loading bar
-def loading_bar(interval):
-    for i in tqdm(range(10)):
-        time.sleep(interval) #how fast to load the bar, jumps in x seconds
